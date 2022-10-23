e15854af82ec6656b342fa24d3870ff8ffb7cfb0
Everton Bueno da silva <evertonbuenno2014@outlook.com>
2022-10-23 20:18:52 -0300

    Data annotations

--- Crud Simples/Models/Usuario.cs
+++ Crud Simples/Models/Usuario.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Crud_Simples.Models
 {
@@ -6,13 +8,22 @@ namespace Crud_Simples.Models
     {
         [Key]
         public int Id { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "O nome é obrigatorio!")]
+        [Column(TypeName = "nvarchar(12)")]
         public string Nome { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "O sobrenome é obrigatorio!")]
+        [Column(TypeName = "nvarchar(22)")]
         public string Sobrenome { get; set; }
-        [Required]
-        public int Idade { get; set; }
-        [Required]
+
+        [Column]
+        [Range(1,199, ErrorMessage ="Idade não permitida")]
+        public string Idade { get; set; }
+
+        [Column(TypeName ="nvarchar(40)")]
+        [Required(ErrorMessage = "O email é obrigatorio!")]
+        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$")]
         public string Email { get; set; }
     }
 }
