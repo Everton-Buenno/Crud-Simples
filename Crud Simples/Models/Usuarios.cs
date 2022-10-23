7ae0c46b53be435e694ee5928329127cb1f5145e
Everton Bueno da silva <evertonbuenno2014@outlook.com>
2022-10-23 19:40:36 -0300

    User Model

---
+++ Crud Simples/Models/Usuarios.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Crud_Simples.Models
+{
+    public class Usuarios
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        [Required]
+        public string Sobrenome { get; set; }
+        [Required]
+        public int Idade { get; set; }
+        [Required]
+        public string Email { get; set; }
+    }
+}
