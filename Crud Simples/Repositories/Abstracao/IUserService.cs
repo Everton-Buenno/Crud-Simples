017998e3dc7d252d788505d9ea9fe6d3ec8cf20e
Everton Bueno da silva <evertonbuenno2014@outlook.com>
2022-10-23 21:12:37 -0300

    Criação e implementação das interfaces.

---
+++ Crud Simples/Repositories/Abstracao/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using Crud_Simples.Models;
+
+namespace Crud_Simples.Repositories.Abstracao
+{
+    public interface IUserService
+    {
+
+
+        bool Add(Usuario model);
+
+        bool Update(Usuario model);
+        bool Delete(int id);
+
+        Usuario ListById(int id);
+
+        IEnumerable<Usuario> ListUser();
+    }
+}
---
+++ Crud Simples/Repositories/Implementacoes/UserService.cs
@@ -0,0 +1,73 @@
+﻿using Crud_Simples.Data;
+using Crud_Simples.Models;
+using Crud_Simples.Repositories.Abstracao;
+
+namespace Crud_Simples.Repositories.Implementacoes
+{
+    public class UserService : IUserService
+
+    {
+
+        private readonly DataContext context;
+
+
+        public UserService(DataContext context)
+        {
+            this.context = context;
+        }
+
+
+
+        public bool Add(Usuario model)
+        {
+            try
+            {
+                context.Usuarios.Add(model);
+                context.SaveChanges();
+                return true;
+            }catch(Exception Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool Delete(int id)
+        {
+            try
+            {
+                var data = this.ListById(id);
+                if(data == null)
+                    return false;
+                context.Usuarios.Remove(data);
+                return true;
+            }catch(Exception Exception)
+            {
+                return false;
+            }
+        }
+
+        public Usuario ListById(int id)
+        {
+            return context.Usuarios.Find(id);
+        }
+
+        public IEnumerable<Usuario> ListUser()
+        {
+            return context.Usuarios.ToList();
+        }
+
+        public bool Update(Usuario model)
+        {
+            try
+            {
+                context.Usuarios.Update(model);
+                context.SaveChanges();
+                return true;
+            }
+            catch(Exception Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
